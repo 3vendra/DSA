1ee8fb9c340862c51eb1c7869e9553eea9972619
3vendra <118194780+3vendra@users.noreply.github.com>
2024-05-23 13:01:42 +0530

    Create 121Leetcodebruteforce.py

---
+++ 121Leetcodebruteforce.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        Maxprofit=0
+        for i in range(len(prices)-1):
+            for j in range(i+1,len(prices)):
+                profit=prices[j]-prices[i]
+                print(i,j,profit,Maxprofit)
+                if(profit>Maxprofit):
+                    Maxprofit=profit
+        return Maxprofit
+#output time limit exceed time complexity n2 need to improve
